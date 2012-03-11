fee175efd8bb9a9b9716fcf90fcd246303bc6a30
Aaron Daniel <amdtech@me.com>
2012-03-11 16:10:37 -0700

    hook:hook_version doesn't really make sense

--- lib/central-core/version.rb
+++ lib/central-core/version.rb
@@ -1,9 +1,9 @@
 # Give Central access to my version info
 
 module CentralCore
-  def self.hook_version
+  def self.version
     VERSION
   end
 end
 
-Central.register_hook(:hook_version, CentralCore)
+Central.register_hook(:version, CentralCore)
